11d231da5dff94a9a95a54eeb1857a445ae688a5
zahrasabill <zahrasabilla30@gmail.com>
2025-02-23 12:24:25 +0700

    up repo baru

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header v-if="$route.name != 'login'">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item me-3">
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <a href="#">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  </header>
+  
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,41 @@
+<template lang="">
+    <div class="container">
+    <div class="d-flex vh-100 bg-light justify-content-center align-items-center">
+        <div class="card p-4 shadow" style="width: 350px;">
+            <h3 class="text-center">Login</h3>
+            <div class="card-body">
+            <form @submit.prevent>
+                <div class="form-group mb-3">
+                    <label for="email">Email</label>
+                    <input type="email" v-model="email" class="form-control" id="email" placeholder="Masukkan email" required>
+                </div>
+                <div class="form-group mb-3">
+                    <label for="password">Password</label>
+                    <input type="password" v-model="password" class="form-control" id="password" placeholder="Masukkan password" required>
+                </div>
+                <button @click="login()" class="btn btn-primary btn-block form-control">Login</button>
+            </form>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            email: "",
+            password: ""
+        }
+    },
+    methods: {
+        login(){
+            console.log(this.email)
+            console.log(this.password)
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
