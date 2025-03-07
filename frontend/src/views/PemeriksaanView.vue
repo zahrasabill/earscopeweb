9ba1326746949e5a6212053fbc6fca8054652795
zahrasabill <zahrasabilla30@gmail.com>
2025-03-07 16:38:56 +0700

    p

---
+++ frontend/src/views/PemeriksaanView.vue
@@ -0,0 +1,27 @@
+<template>
+    <admin-layout active-page="pemeriksaan">
+    <div>
+      <h2>Pemeriksaan View</h2>
+      
+    </div>
+    </admin-layout>
+  </template>
+  
+  <script>
+import AdminLayout from '@/components/AdminLayout.vue';
+import { Modal } from 'bootstrap';
+
+export default {
+  name: "PemeriksaanView",
+  components: {
+    AdminLayout
+  }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: black;
+  }
+  </style>
+  
