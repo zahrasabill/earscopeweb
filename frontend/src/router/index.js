6a2a96c9282e160b0fc32629816c919385053c9e
zahrasabill <zahrasabilla30@gmail.com>
2025-03-07 16:47:50 +0700

    update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,7 +7,7 @@ import RegisterView from '@/views/RegisterView.vue'
 import PasienView from '@/views/PasienView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
